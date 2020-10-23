c93b6e9724d4410556ba212fb32161485f5582ac
Karol Krawczyk <karol.krawczyk1@edu.uekat.pl>
2020-10-23 21:14:18 +0200

    added GUI and image preparation module

---
+++ app.py
@@ -0,0 +1,71 @@
+import tkinter as tk
+import pickle
+import numpy as np
+import os
+import io
+from PIL import Image
+from image_preprocessing import img_to_mnist
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+WIDTH = 400
+HEIGHT = 350
+with open('ANN/model.h5', 'rb') as file:
+    MODEL = pickle.load(file)               # deserialization of the Neural Network model
+
+class App:
+    def __init__(self):
+        self.main()
+
+    def main(self):
+        root = tk.Tk()
+        root.title('Handwritten digits recognition')
+        root.geometry(f'{WIDTH}x{HEIGHT}')
+        root.resizable(False,False)
+
+        global canvas
+        canvas = tk.Canvas(root, width=WIDTH, height=0.7 * HEIGHT)
+        canvas.grid(row=0, column=0, columnspan=2)
+        canvas.old_coords = None
+
+        clear_button = tk.Button(root, text='CLEAR', font=('Arial', 14), command=lambda x=canvas: x.delete('all'))
+        clear_button.grid(row=1, column=0, pady=30)
+
+        submit_button = tk.Button(root, text='CHECK', font=('Arial', 14), command=self.run)
+        submit_button.grid(row=1, column=1, pady=30)
+
+        root.bind('<B1-Motion>', self.draw)
+        root.bind('<ButtonRelease-1>', self.reset_coords)
+
+        root.mainloop()
+
+    def draw(self, event):
+        x, y = event.x, event.y
+        if canvas.old_coords:
+            x1, y1 = canvas.old_coords
+            canvas.create_oval(x, y, x1, y1, width=15)
+        canvas.old_coords = x, y
+
+    def reset_coords(self, event):
+        canvas.old_coords = None
+
+    def save_drawing(self):
+        ps = canvas.postscript(colormode='gray')
+        img = Image.open(io.BytesIO(ps.encode('utf-8')))  # canvas screenshot
+
+        img.save('IMG.jpg', 'jpeg')
+
+    def predict_number(self):
+        image = np.array(img_to_mnist('IMG.jpg')).reshape((1, 28, 28))
+        predicted_values = MODEL.predict(image)  # probability distribution for digits from 0-9
+
+        for number in range(10):
+            probability = predicted_values[0][number]
+            print('Probability for {}:\t{:.5f}'.format(number, probability))
+        print(f'Predicted: {np.argmax(predicted_values)}')
+        os.remove('IMG.jpg')                # deletes the drawing after prediction
+
+    def run(self):
+        self.save_drawing()
+        self.predict_number()
+
+App()
