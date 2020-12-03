45854ebec424ba7417d1e24736cc0440b3f1540b
Karol Krawczyk <karol.krawczyk1@edu.uekat.pl>
2020-12-03 01:51:56 +0100

    transformed the Neural Network to CNN

--- ANN/network.py
+++ ANN/network.py
@@ -1,46 +1,49 @@
-from keras import *
+import numpy as np
+from keras import utils,layers,models, datasets,callbacks
 import pickle
 import os
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'  # ignores the warning about CPU Tensorflow configuration.
 
+BATCH_SIZE = 32
+EPOCHS = 3
+CALLBACK = callbacks.EarlyStopping(monitor='val_loss', patience=2)
+
 
 class NeuralNetwork:
     """ The Neural Network class. """
 
-    def __init__(self, X, y):
-        """
-        :param X: vector with training images data for the model to train on
-        :param y: vector of labels of training images
-        """
-        self.train_images = X
-        self.train_labels = y
-
-
     def get_model(self):
         """
-        Creates, trains and returns the Neural Network.
-        :return: Sequential object trained on the data passed to the class constructor. Model score on test set ~97.5%
+        Creates, trains and returns the Convolutional Neural Network.
+        :return: Compiled CNN model. Model score on test set ~97.5%
         """
-        model = Sequential([
-            layers.Flatten(),
-            layers.Dense(128, activation='relu'),  # 1st layer with 128 neurons and rectifier activation function
-            layers.Dense(128, activation='relu'),  # 2nd layer with 128 neurons and rectifier activation function
-            layers.Dense(64, activation='relu'),  # 3rd layer with 128 neurons and rectifier activation function
-            layers.Dense(10, activation='softmax')  # softmax optimization function for probability distribution
-        ])
 
+        model = models.Sequential([
+                 layers.Conv2D(filters=64, kernel_size=3, input_shape = (28,28,1), padding='same', activation='relu'),
+                 layers.MaxPooling2D(pool_size=(2,2), strides=2),
+                 layers.Conv2D(filters=32, kernel_size = 3, padding='same', activation='relu'),
+                 layers.Flatten(),
+                 layers.Dense(units = 128, activation='relu'),
+                 layers.Dense(units=10, activation='softmax')
+])
         model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
-        model.fit(self.train_images, self.train_labels, epochs=6)
-
         return model
 
 
-(train_images, train_labels), (test_images, test_labels) = datasets.mnist.load_data()  # loading MNIST dataset
-train_images = utils.normalize(train_images, axis=1)  # normalizing images between 0-1 range
-test_images = utils.normalize(test_images, axis=1)
+(train_images, train_labels), (test_images, test_labels) = datasets.mnist.load_data()               # loading MNIST dataset
 
-model = NeuralNetwork(train_images, train_labels).get_model()
+train_shape, test_shape = train_images.shape[0], test_images.shape[0]
+train_images = utils.normalize(train_images, axis=1).reshape(train_shape,28,28,1)                   # normalizing images between 0-1 range
+test_images = utils.normalize(test_images, axis=1).reshape(test_shape,28,28,1)
+
+
+model = NeuralNetwork().get_model()
+model.fit(train_images, train_labels,
+          batch_size=BATCH_SIZE,
+          epochs=EPOCHS,
+          validation_data=[test_images, test_labels],
+          callbacks=[CALLBACK])
 
 try:
     with open('model.h5', 'wb') as file:
